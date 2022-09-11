f3cb3ee2c4a247219174f48e10346e11d2c4a1e8
Proloycb <proloychacroborty@gmail.com>
2022-09-11 11:00:47 +0600

    login implement

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-
 import { Box } from '@mui/material';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Navbar from './pages/Shared/Navbar';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Route, Routes } from 'react-router-dom';
+import Signup from './pages/Login/Signup';
 
 function App() {
   return (
     <Box>
       <Navbar/>
-      <Home/>
-      <Login/>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='login' element={<Login />}/>
+        <Route path='/signup' element={<Signup />}/>
+      </Routes>
+      <ToastContainer />
     </Box>
   );
 }
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,6 +2,10 @@ import { Avatar, Button, Grid, Paper, TextField, Typography } from '@mui/materia
 import React, { useState } from 'react';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { Link } from 'react-router-dom';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { toast } from 'react-toastify';
+import Loading from '../Shared/Loading';
 
 const Login = () => {
     const [email, setEmail] = useState('');
@@ -9,12 +13,34 @@ const Login = () => {
     const paperStyle = { padding: "20px", height: '60vh', width: 280, margin: "20px auto" }
     const avatarStyle = { backgroundColor: '#1bbd7e' }
     const btnStyle = { margin: '8px 0' }
-
-    const handleSubmit = () => {
-        
-        console.log(email)
+    let signInError;
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+    if (loading || gLoading || sending) {
+        return <Loading />
     }
-    const handleReset = () => {}
+    if (error || gError) {
+        signInError = <p sx={{color: 'red'}}><small>{error?.message || gError?.message}</small></p>
+    }
+    const handleSubmit = async () => {
+        await signInWithEmailAndPassword(email, password);
+        toast.success('Login successful')
+    }
+    const handleReset = async () => {
+        if (email) {
+            await sendPasswordResetEmail(email);
+            toast.success('Sent email')
+        }
+        else {
+            toast.error('Please enter your email')
+        }
+    }
     return (
         <Grid>
             <Paper elevation={20} style={paperStyle}>
@@ -23,17 +49,18 @@ const Login = () => {
                     <h2>Sign In</h2>
                 </Grid>
                 <TextField onChange={(e) => setEmail(e.target.value)} variant="standard" label='Username' placeholder='Enter username' fullWidth required sx={{marginBottom: "10px"}}/>
-                <TextField onChange={(e) => setEmail(e.target.value)} variant="standard" label='Password' placeholder='Enter password' type='password' fullWidth required />
+                <TextField onChange={(e) => setPassword(e.target.value)} variant="standard" label='Password' placeholder='Enter password' type='password' fullWidth required />
                 <Typography sx={{marginTop: "10px"}}>Forgot password ? 
                 <Link style={{textDecoration: 'none'}} to="" onClick={() => handleReset()}>Reset Password</Link>
                 </Typography>
+                {signInError}
                 <Button onClick={() => handleSubmit()} type='submit' color='primary' variant="contained" style={btnStyle} fullWidth>Sign in</Button>
                 <Typography > Do you have an account ?
                     <Link style={{textDecoration: 'none'}} to="/signup" >
                         Sign Up
                     </Link>
                 </Typography>
-                <Button color='primary' variant="contained" style={btnStyle} fullWidth>Google Sign in</Button>
+                <Button onClick={() => signInWithGoogle()} color='primary' variant="contained" style={btnStyle} fullWidth>Google Sign in</Button>
             </Paper>
         </Grid>
     );

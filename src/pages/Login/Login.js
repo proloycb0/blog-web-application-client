f6677fd8beb239118de557d4daba638471b6838f
Proloycb <proloychacroborty@gmail.com>
2022-09-11 10:33:37 +0600

    login and signup page design

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,31 +1,39 @@
-import { Avatar, Button, Checkbox, FormControlLabel, Grid, Link, Paper, TextField, Typography } from '@mui/material';
-import React from 'react';
+import { Avatar, Button, Grid, Paper, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-    const paperStyle = { padding: "20px", height: '70vh', width: 280, margin: "20px auto" }
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const paperStyle = { padding: "20px", height: '60vh', width: 280, margin: "20px auto" }
     const avatarStyle = { backgroundColor: '#1bbd7e' }
     const btnStyle = { margin: '8px 0' }
+
+    const handleSubmit = () => {
+        
+        console.log(email)
+    }
+    const handleReset = () => {}
     return (
         <Grid>
-            <Paper elevation={10} style={paperStyle}>
+            <Paper elevation={20} style={paperStyle}>
                 <Grid align='center'>
                     <Avatar style={avatarStyle}><LockOutlinedIcon /></Avatar>
                     <h2>Sign In</h2>
                 </Grid>
-                <TextField label='Username' placeholder='Enter username' fullWidth required />
-                <TextField label='Password' placeholder='Enter password' type='password' fullWidth required />
-                <Button type='submit' color='primary' variant="contained" style={btnStyle} fullWidth>Sign in</Button>
-                <Typography >
-                    <Link href="#" >
-                        Forgot password ?
-                    </Link>
+                <TextField onChange={(e) => setEmail(e.target.value)} variant="standard" label='Username' placeholder='Enter username' fullWidth required sx={{marginBottom: "10px"}}/>
+                <TextField onChange={(e) => setEmail(e.target.value)} variant="standard" label='Password' placeholder='Enter password' type='password' fullWidth required />
+                <Typography sx={{marginTop: "10px"}}>Forgot password ? 
+                <Link style={{textDecoration: 'none'}} to="" onClick={() => handleReset()}>Reset Password</Link>
                 </Typography>
+                <Button onClick={() => handleSubmit()} type='submit' color='primary' variant="contained" style={btnStyle} fullWidth>Sign in</Button>
                 <Typography > Do you have an account ?
-                    <Link href="#" >
+                    <Link style={{textDecoration: 'none'}} to="/signup" >
                         Sign Up
                     </Link>
                 </Typography>
+                <Button color='primary' variant="contained" style={btnStyle} fullWidth>Google Sign in</Button>
             </Paper>
         </Grid>
     );
